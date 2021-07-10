0cb7c5bc5550aabb43d2512f2306b5dc44815612
Sudheerkumarkaspa <sudheerkumarkaspa@gmail.com>
2021-07-10 23:50:41 +0530

    Revert "Dev"

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var productRouter = require("./product");
-var warehouseRouter = require("./warehouse");
-var app = express();
-
-app.use("/warehouse/", warehouseRouter);
-app.use("/product/", productRouter);
-module.exports = app;
